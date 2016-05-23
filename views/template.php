ec60901bcb11a3ddd8bbc6da742e0ee92c92d3e0
Franziska <franziska@yootheme.com>
2016-05-23 12:10:41 +0200

    typo

--- views/template.php
+++ views/template.php
@@ -46,7 +46,7 @@
         </section>
         <?php endif; ?>
 
-        <div id="tm-main" class="tm-main uk-grid data-uk-grid-match" data-uk-grid-margin>
+        <div id="tm-main" class="tm-main uk-grid" data-uk-grid-match data-uk-grid-margin>
 
             <main class="<?= $view->position()->exists('sidebar') ? 'uk-width-medium-3-4' : 'uk-width-1-1'; ?>">
                 <?= $view->render('content') ?>
